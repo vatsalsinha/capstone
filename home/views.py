1d7547f2c075eaeb388eb79618ff12f4c55f07b4
vatsal <vatsalsinha001@gmail.com>
2021-06-13 01:24:34 +0530

    login signup done

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.template import RequestContext
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	return render(request, "index1.html", {}) # string of html code
