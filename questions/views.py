4238c084ebb39f169b9b680553e51280e995702f
vatsal <vatsalsinha001@gmail.com>
2021-06-25 00:08:48 +0530

    fixed small bugs

--- questions/views.py
+++ questions/views.py
@@ -14,7 +14,11 @@ def index(request):
     
 @login_required(login_url = "/accounts/login")
 def questionDetail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+    # question = get_object_or_404(Question, pk = question_id)
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        return redirect('questions:index')
     return render(request, 'questions/questiondetail.html', {'question': question})
 
 
@@ -30,6 +34,6 @@ def vote(request, question_id):
     else:
         vote = Vote(user=request.user, question=question, choice=option_selected)
         vote.save()
-    return HttpResponseRedirect(reverse('questions:detail', args=(question_id,))) # does '/polls/id/results/'
+    return HttpResponseRedirect(reverse('questions:detail', args=(question_id+1,))) # does '/polls/id/results/'
 
     
